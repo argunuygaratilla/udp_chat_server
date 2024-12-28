914bd6394e7b641bb5aa0f5b2bea3e711e3dd403
uygar atilla argun <uygaratillaargun@yandex.com>
2024-12-29 01:12:50 +0300

    notification sound added

--- client.py
+++ client.py
@@ -1,27 +1,29 @@
 import socket
 import threading
 import random
+import playsound  # Bildirim sesi için
 
-client=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.bind(("localhost",random.randint(8000,9000)))
-name=input("Nickname: ")
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client.bind(("localhost", random.randint(8000, 9000)))
+name = input("Nickname: ")
 
-def recieve():
+def receive():
     while True:
         try:
-            mesaage,_=client.recvfrom(1024)
-            print(mesaage.decode())
+            message, _ = client.recvfrom(1024)
+            print(message.decode())
+            playsound.playsound('notification_sound.mp3', block=False)  # Ses dosyasını çalar
         except:
             pass
 
-t=threading.Thread(target=recieve)
+t = threading.Thread(target=receive)
 t.start()
 
-client.sendto(f"SIGNUP_TAG:{name}".encode(),("localhost",9999))
+client.sendto(f"SIGNUP_TAG:{name}".encode(), ("localhost", 9999))
 
 while True:
-    message=input("")
-    if message=="!q":
+    message = input("")
+    if message == "!q":
         exit()
     else:
-        client.sendto(f"{name}: {message}".encode(), ("localhost",9999))
+        client.sendto(f"{name}: {message}".encode(), ("localhost", 9999))
