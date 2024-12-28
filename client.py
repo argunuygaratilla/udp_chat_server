47636eac137becd99583e44304129980c14c0fbf
uygar atilla argun <uygaratillaargun@yandex.com>
2024-12-29 01:18:47 +0300

    replied message has been changed

--- client.py
+++ client.py
@@ -22,9 +22,14 @@ def receive():
     while True:
         try:
             message, _ = client.recvfrom(1024)
-            print(message.decode())
-            notification_sound.play()  # Yeni bir mesaj alındığında ses çal
-        except:
+            message_decoded = message.decode()
+
+            # Eğer mesaj kendi kullanıcı adınızdan geldiyse, mesajı yazdırma
+            if not message_decoded.startswith(f"{name}:"):
+                print(message_decoded)
+                notification_sound.play()  # Yeni bir mesaj alındığında ses çal
+
+        except Exception as e:
             pass
 
 # Mesajları almak için ayrı bir thread başlat
@@ -39,4 +44,5 @@ while True:
     if message == "!q":
         exit()
     else:
+        # Gönderilen mesajı yalnızca başkalarına gönder, ekrana basma
         client.sendto(f"{name}: {message}".encode(), ("localhost", 9999))
